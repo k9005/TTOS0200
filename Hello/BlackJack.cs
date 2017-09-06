6f56bd901b43ea86368bbd266520c263e49f98fd
K9005 <K9005@ELEK-436-WS-24>
2017-09-06 10:57:44 +0300

    Labrat project added for week tasks

--- Hello/BlackJack.cs
+++ Hello/BlackJack.cs
@@ -13,14 +13,23 @@ namespace Hello
         int myNumber = 17;
         int theirNumber;
         System.Console.WriteLine("*** BlackJack! ***");
-        System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-        //reading and converting 
-        theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-        //comparing that given umber is valid
-            if (theirNumber < 1 || theirNumber > 21)
+        do //infinite game times
+        {
+            do //If number is not between 1-21 program asks number again
             {
-                Console.WriteLine("The given number is out of limits, try again.");
-            }
+                    System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+            //reading and converting 
+            
+            
+                theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+            //comparing that given umber is valid
+            
+
+            
+        if (theirNumber < 1 || theirNumber > 21)
+        {
+           Console.WriteLine("The given number is out of limits, try again.");
+        }
             
         else
         {
@@ -34,7 +43,9 @@ namespace Hello
                 System.Console.WriteLine("You lose.");
             }
                 
-            }
-    }
+        }
+        } while (theirNumber < 1 || theirNumber > 21);
+        } while (true);
+        }
 }
 }
---
+++ Labrat/Lab01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Olio-ohjelmoinnin viikkotehtävät Labra 1
+//Lauri Rämö
+namespace Labrat
+{
+    class Lab01
+    {
+        public static void Tehtava01 ()
+        {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku". 
+            Console.WriteLine("getting there..");
+
+        }
+    }
+}
