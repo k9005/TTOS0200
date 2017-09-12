873f1f4d9df075297028c8ac9691f751b07bc815
Olympos <Olympos@DESKTOP-43PGONO>
2017-09-12 23:53:01 +0300

    tehtavat 17 ja 20 jäljellä

--- Labrat/Lab01.cs
+++ Labrat/Lab01.cs
@@ -334,7 +334,49 @@ namespace Labrat
 
         public static void Tehtava19()
         {
+            string sana = "putkea";
 
+            
+            char[] a = new char[sana.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = '_';
+            }
+            Console.WriteLine(".....HIRSIPUU.....");
+            Console.Write("Sanasi on: ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + "  ");
+            }
+            Console.WriteLine();
+
+            int luku = 0;
+            do
+            {
+                Console.Write("Anna kirjain: ");
+                char input = Console.ReadLine().ToCharArray()[0];
+
+                for (int i = 0; i < sana.Length; i++)
+                {
+                    
+                    if (sana[i] == input)
+                    {
+                        luku++; 
+                        a[i] = input;  
+
+                        
+                        for (int j = 0; j < a.Length; j++)
+                        {
+                            Console.Write(a[j] + " ");
+                        }
+                    }
+                }
+                
+            }
+
+            while (luku < a.Length);
+            Console.WriteLine("You guessed it right");
         }
      
     
