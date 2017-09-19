49dc6bc84828195cbd7a9179cb430b16fa981973
Olympos <Olympos@DESKTOP-43PGONO>
2017-09-19 23:50:04 +0300

    Tehtava 5 tehty

---
+++ ConsoleApp1/ClassOpiskelija.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opiskelija
+{
+    class Classopiskelija
+    {
+        public string Nimi { get; set; }
+        public string id { get; set; }
+        public int ika { get; set; }
+        public string sposti { get; set; }
+        public string osoite { get; set; }
+        public Classopiskelija(string Nimi, string opiskelijatunn, int ikä, string sposti, string osoite) : base()
+        {
+            this.Nimi = Nimi;
+            this.id = id;
+            this.ika = ika;
+            this.sposti = sposti;
+            this.osoite = osoite;
+
+        }
+        public Classopiskelija()
+        {
+        }
+        public string tiedot()
+        {
+            return Nimi + ", " + ika + ", " + id + ", " + sposti + ", " + osoite;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opiskelija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] opiskelijat = new string[2];
+
+            for (int i = 0; i < opiskelijat.Length; i++)
+            {
+                Classopiskelija uusiop = new Classopiskelija();
+                Console.WriteLine("Lisää Opiskelija!");
+
+                Console.Write("Nimi: ");
+                uusiop.Nimi = Console.ReadLine();
+
+                Console.Write("Ikä: ");
+                uusiop.ika = int.Parse(Console.ReadLine());
+
+                Console.Write("Opiskelijatunnus: ");
+                uusiop.id = Console.ReadLine();
+
+                Console.Write("Sähköposti: ");
+                uusiop.sposti = Console.ReadLine();
+
+                Console.Write("Kotiosoite: ");
+                uusiop.osoite = Console.ReadLine();
+
+                opiskelijat[i] = uusiop.Nimi + ", " + uusiop.ika + ", " + uusiop.id + ", " + uusiop.sposti + ", " + uusiop.osoite;
+
+                
+            }
+            Console.WriteLine("\n\nOpiskelijat ja tiedot!\n ");
+            
+            for (int i = 0; i < opiskelijat.Length; i++)
+            {
+                Console.WriteLine(opiskelijat[i]);
+                Console.WriteLine(".....................");
+            }
+
+        }
+    }
+}
