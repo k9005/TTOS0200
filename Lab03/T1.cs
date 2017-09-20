3f1c05613596493d29546ff7047e7c2f1f818190
K9005 <K9005@ELEK-436-WS-24>
2017-09-20 11:40:22 +0300

    Tehtava 1 osa 1 tehty.

---
+++ Lab03/T1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    public static class T1
+    {
+        //static bool numero { get; set; }
+        //static bool paiva { get; set; }
+        public static bool Onkoluku(string syote)
+        {
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(syote);
+            foreach (byte b in asciiBytes)
+            {
+                if (b > 48 && b < 57 || b == 44) return true;
+            }
+
+            return false;
+        }
+        public static bool Onkopvm(string syote)
+        {
+            return false;
+        }
+    }
+}
