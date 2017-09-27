88e8ec6338ff8a4831e6573e34d2ab793166905a
K9005 <K9005@ELEK-436-WS-24>
2017-09-27 10:30:19 +0300

    Tehtava 04.1 tehty

---
+++ Lab04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Employee employee = new Employee();
+            employee.FirstName = "Esa";
+            employee.LastName = "Nahka";
+            employee.Salary = 1200;            
+            Console.WriteLine(employee.ToString());
+                       
+            Boss boss = new Boss();
+            boss.FirstName = "Tero";
+            boss.LastName = "Nuppi";
+            boss.Salary = 10000;
+            boss.Car = "Mercedes-Benz";
+            boss.Bonus = 1000000;            
+            Console.WriteLine(boss.ToString());
+
+            
+            
+            
+            
+        }
+    }
+}
---
+++ Lab04/Tehtavat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Profession { get; set; }
+        public int Salary { get; set; }
+                
+        public Employee()
+        {
+        }
+
+        
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public void EmployeeMethod()
+        {
+            Console.WriteLine("This method belongs to Employee!");
+        }
+
+        
+        public override string ToString()
+        {
+            return "Name: "+ FirstName + " " + LastName + " Profession: " + Profession + " Salary: " + Salary+ "$" ;
+        }
+    }
+    class Boss : Employee
+    {
+        public string Car { get; set; }
+        public int Bonus { get; set; }
+               
+        public Boss()
+        {
+        }
+                
+        public Boss(string firstName, string lastName, string car)
+            : base(firstName, lastName)
+        {
+            Car = car;
+        }
+                 
+        public void BossMethod()
+        {
+            Console.WriteLine("This method belongs to Boss!");
+        }
+                
+        public override string ToString()
+        {
+            return base.ToString() + " Car: " + Car + "Bonus: " + Bonus+ "$";
+        }
+    }
+}
