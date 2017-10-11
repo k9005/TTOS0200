3eb7ddaea784ff26d2bd759c9ff1e1fae6fc687b
K9005 <K9005@ELEK-436-WS-24>
2017-10-11 10:43:06 +0300

    tehtavia

---
+++ Lab6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.MiniASIO
+{
+    class TestaaMiniASIO
+    {
+        static void Testaa()
+        {
+            List<Opiskelija> opiskelijat = new List<Opiskelija>();
+            Opiskelija masa = new Opiskelija("Masa", "Niemi");
+            opiskelijat.Add(masa);
+            opiskelijat.Add(new Opiskelija("Muammar", "Bombom", "He123"));
+            //3 vaihtoehto
+            opiskelijat.Add(new Opiskelija { Etunimi = "Lucifer", Sukunimi = "Satan", AsioID = "B666" });
+            opiskelijat.Add(new Opiskelija { Etunimi = "Hurri", Sukunimi = "Ruotsalainen", AsioID = "F6969", Ryhma = "Kysymysmerkkimiehet" });
+            Opiskelija kalle = new Opiskelija();
+            kalle.Etunimi = "Kalle";
+            kalle.Sukunimi = "Pikku";
+            kalle.AsioID = "T5498";
+            opiskelijat.Add(kalle);
+
+            //yhdenopiskelijan näyttäminen
+            Console.Write("Anna numero 1-5 väliltä: ");
+            int i = int.Parse(Console.ReadLine());
+            
+            if (i-1 > -1 && i-1 < opiskelijat.Count)
+                Console.WriteLine("MiniASIOn {0} opiskelija on {1}", i, opiskelijat[i-1].ToString());
+            else
+                Console.WriteLine("MiniAsiossa on vain {0} opiskelijaa!!!", opiskelijat.Count);
+            //Kaikki opiskelijat
+            Console.WriteLine("MiniASIOn kaikki opiskelijat: ");
+            foreach (var o in opiskelijat)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            //Kaikki opiskelijat aakkosjärjestyksessä
+            opiskelijat.Sort((x, y) => x.Sukunimi.CompareTo(y.Sukunimi));
+            Console.WriteLine("MiniASIOn kaikki opiskelijat aakkosjärjestyksessä: ");
+            foreach (var o in opiskelijat)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Testaa();
+        }
+    }
+}
