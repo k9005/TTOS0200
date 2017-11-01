d80b8d2475d0cae066bf47f7119719389dfafbb3
K9005 <K9005@ELEK-436-WS-24>
2017-11-01 08:39:36 +0200

    bo

---
+++ Lab6/Opiskelija.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.MiniASIO
+{
+    public class Opiskelija
+    {
+        #region PROPERTIES
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string AsioID { get; set; }
+        public string Ryhma { get; set; }
+        #endregion
+
+        #region CONSTRUCTORS
+        public Opiskelija()
+        { }
+        public Opiskelija(string etunimi, string sukunimi)
+        {
+            Etunimi = etunimi;
+            Sukunimi = sukunimi;
+        }
+        public Opiskelija(string etunimi, string sukunimi, string asioid): this(etunimi, sukunimi)
+        {
+            AsioID = asioid;
+        }
+        public Opiskelija(string etunimi, string sukunimi, string asioid, string ryhma) : this(etunimi, sukunimi, asioid)
+        {
+            Ryhma = ryhma;
+        }
+        #endregion
+
+        #region METHODS
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", Etunimi, Sukunimi, AsioID, Ryhma);
+        }
+        #endregion
+
+    }
+}
