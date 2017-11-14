f02cb985a1248de4fbfec07721a146bdad8c0371
Olympos <Olympos@DESKTOP-43PGONO>
2017-11-14 23:28:17 +0200

    Lab 10 T1,2,3 tehty.

--- Lab 10.2/MainWindow.xaml.cs
+++ Lab 10.2/MainWindow.xaml.cs
@@ -24,9 +24,11 @@ namespace Lab_10._2
         {
             InitializeComponent();
         }
-        double number;
+        
         double number2;        
         double kurssi = 0;
+        string eur = "kurssi: 0.8997";
+        string Usd = "Kurssi: 1.1003";
         private void Amount1_TextChanged(object sender, TextChangedEventArgs e)
         {
             double number = 0;
@@ -37,8 +39,8 @@ namespace Lab_10._2
             else
             {
                 if (Combo1.Text == "Euro" && Combo2.Text == "Euro") kurssi = 1;
-                else if (Combo1.Text == "Euro" && Combo2.Text == "Yhdysvallat USD") kurssi = 0.8997;
-                else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Euro") kurssi = 1.1003;
+                else if (Combo1.Text == "Euro" && Combo2.Text == "Yhdysvallat USD") kurssi = 1.1003;
+                else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Euro") kurssi = 0.8997;
                 else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Yhdysvallat USD") kurssi = 1;
                 else kurssi = 0;
                 number = double.Parse(Amount1.Text);
@@ -54,8 +56,20 @@ namespace Lab_10._2
 
         private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
-            
+            ListBoxItem listitem = (ListBoxItem)Combo1.SelectedItem;
+            if ((ListBoxItem)Combo1.SelectedItem == euro)
+            {
+                txtblo1.Text = eur.ToString();
+            }
+            else if ((ListBoxItem)Combo1.SelectedItem == usd)
+            {
+                txtblo1.Text = Usd.ToString();
+            }
+            else
+            {
+                txtblo1.Text = "Kohde valuutta";
+            }
+
         }
 
         private void Combo2_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ Lab 10.3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_10._3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double ala = double.Parse(ikkunanleveys.Text) * double.Parse(ikkunankorkeus.Text);
+            ikkunanpinta_ala.Text = ala.ToString()+"cm^2";            
+            double lasiala = (double.Parse(ikkunanleveys.Text) - double.Parse(karminleveys.Text)*2) * (double.Parse(ikkunankorkeus.Text) - double.Parse(karminleveys.Text)*2);
+            lasinpinta_ala.Text = lasiala.ToString()+"cm^2";
+            double piiri = ((double.Parse(ikkunanleveys.Text) - double.Parse(karminleveys.Text) * 2)*2) + ((double.Parse(ikkunankorkeus.Text) - double.Parse(karminleveys.Text) * 2)*2);
+            karminpiiri.Text = piiri.ToString() + "cm";
+        }
+    }
+}
