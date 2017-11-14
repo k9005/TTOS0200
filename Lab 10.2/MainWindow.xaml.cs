2950ec50a1d96519eb7a12faff4a6db7ae2e7f59
Olympos <Olympos@DESKTOP-43PGONO>
2017-11-14 19:09:15 +0200

    fixing

--- Lab 10.2/MainWindow.xaml.cs
+++ Lab 10.2/MainWindow.xaml.cs
@@ -29,19 +29,22 @@ namespace Lab_10._2
         double kurssi = 0;
         private void Amount1_TextChanged(object sender, TextChangedEventArgs e)
         {
+            double number = 0;
+            if (!double.TryParse(Amount1.Text, out number))
+            {
 
-
-            //else
-            //{
-            if (Combo1.Text == "Euro" && Combo2.Text == "Euro") kurssi = 1;
-            else if (Combo1.Text == "Euro" && Combo2.Text == "Yhdysvallat USD") kurssi = 0.8997;
-            else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Euro") kurssi = 1.1003;
-            else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Yhdysvallat USD") kurssi = 1;
-            else kurssi = 0;
-            number = double.Parse(Amount1.Text);
+            }
+            else
+            {
+                if (Combo1.Text == "Euro" && Combo2.Text == "Euro") kurssi = 1;
+                else if (Combo1.Text == "Euro" && Combo2.Text == "Yhdysvallat USD") kurssi = 0.8997;
+                else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Euro") kurssi = 1.1003;
+                else if (Combo1.Text == "Yhdysvallat USD" && Combo2.Text == "Yhdysvallat USD") kurssi = 1;
+                else kurssi = 0;
+                number = double.Parse(Amount1.Text);
                 number2 = number * kurssi;
                 Amount2.Text = number2.ToString("0.00");
-            //}
+            }
         }
 
         private void Amount2_TextChanged(object sender, TextChangedEventArgs e)
